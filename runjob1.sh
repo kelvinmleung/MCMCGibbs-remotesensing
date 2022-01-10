bb9ebb2353a86bfd8b85ebbe7a04d3438e7f5c2b
Kelvin Leung <kmleung@mit.edu>
2022-01-10 14:54:15 -0500

    rungibbs on cluster first time

---
+++ runjob1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=mcm1
+#SBATCH --workdir=/master/home/kmleung/JPLproject/MCMCGibbs-remotesensing
+#SBATCH --output=runjob1.out
+#SBATCH --error=runjob1.err
+#SBATCH --exclusive
+ 
+python runFile.py
